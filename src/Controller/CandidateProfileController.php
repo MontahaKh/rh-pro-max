727296346d1a80ecfc0e326247c9d7f2d96cd26c
Houssem <hooussem.amor@tek-up.de>
2025-12-02 21:28:15 +0100

    fix: Update access control for candidate profiles to restrict access to HR and Recruiters only

--- src/Controller/CandidateProfileController.php
+++ src/Controller/CandidateProfileController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/candidate/profile')]
+#[IsGranted('ROLE_RECRUITER')]
 class CandidateProfileController extends AbstractController
 {
     #[Route('/', name: 'app_candidate_profile_index', methods: ['GET'])]
